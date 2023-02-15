2c181d3cacc927fd06a8854ff9dc08c307297c80
chloe-t <chloe.trouilh@gmail.com>
2023-02-15 14:23:16 +0100

    Rename env to var

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 variable "GOOGLE_APPLICATION_CREDENTIALS" {}
 
 provider "google" {
-  credentials = file("${env.GOOGLE_APPLICATION_CREDENTIALS}")
+  credentials = file("${var.GOOGLE_APPLICATION_CREDENTIALS}")
   project     = "i-need-my-belt"
   region      = "us-west1"
 }
