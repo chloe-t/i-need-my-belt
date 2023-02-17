8d56ff9b889f08ec9e2572bcf8316d3119d6d00b
chloe-t <chloe.trouilh@gmail.com>
2023-02-17 15:41:09 +0100

    Add public key to instace metadata

--- main.tf
+++ main.tf
@@ -86,6 +86,7 @@ resource "google_compute_instance" "default" {
     #ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
     #ssh-keys = "ubuntu:${file("ubuntu.pub")}"
     # ssh-keys          = "ubuntu:${file("ubuntu.pub")}"
+    ssh-keys          = tls_private_key.ephemeral.public_key_openssh
     enable-oslogin    = "TRUE"
     enable-oslogin-sk = "TRUE"
   }
