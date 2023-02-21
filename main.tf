e255860cfdcdf501dcc167ae24f25654a8251904
chloe-t <chloe.trouilh@gmail.com>
2023-02-21 18:10:24 +0100

    reset firewall

--- main.tf
+++ main.tf
@@ -66,78 +66,78 @@ resource "google_compute_network" "default" {
   name = "test-network"
 }
 
-resource "google_compute_firewall" "ssh" {
-  name    = "${local.project_name}-firewall-ssh"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  target_tags   = ["${local.project_name}-firewall-ssh"]
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "http" {
-  name    = "${local.project_name}-firewall-http"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  target_tags   = ["${local.project_name}-firewall-http"]
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "https" {
-  name    = "${local.project_name}-firewall-https"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
-  }
-
-  target_tags   = ["${local.project_name}-firewall-https"]
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "icmp" {
-  name    = "${local.project_name}-firewall-icmp"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  target_tags   = ["${local.project_name}-firewall-icmp"]
-  source_ranges = ["0.0.0.0/0"]
-}
-
-# resource "google_compute_firewall" "default" {
-#   name    = "test-firewall"
+# resource "google_compute_firewall" "ssh" {
+#   name    = "${local.project_name}-firewall-ssh"
 #   network = google_compute_network.default.name
-
-#   allow {
-#     protocol = "icmp"
-#   }
 
 #   allow {
 #     protocol = "tcp"
 #     ports    = ["22"]
 #   }
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80", "8080", "22", "1000-2000"]
-#   }
-
-#   source_tags   = ["web"]
+#   target_tags   = ["${local.project_name}-firewall-ssh"]
 #   source_ranges = ["0.0.0.0/0"]
 # }
+
+# resource "google_compute_firewall" "http" {
+#   name    = "${local.project_name}-firewall-http"
+#   network = google_compute_network.default.name
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["80"]
+#   }
+
+#   target_tags   = ["${local.project_name}-firewall-http"]
+#   source_ranges = ["0.0.0.0/0"]
+# }
+
+# resource "google_compute_firewall" "https" {
+#   name    = "${local.project_name}-firewall-https"
+#   network = google_compute_network.default.name
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["443"]
+#   }
+
+#   target_tags   = ["${local.project_name}-firewall-https"]
+#   source_ranges = ["0.0.0.0/0"]
+# }
+
+# resource "google_compute_firewall" "icmp" {
+#   name    = "${local.project_name}-firewall-icmp"
+#   network = google_compute_network.default.name
+
+#   allow {
+#     protocol = "icmp"
+#   }
+
+#   target_tags   = ["${local.project_name}-firewall-icmp"]
+#   source_ranges = ["0.0.0.0/0"]
+# }
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "22", "1000-2000"]
+  }
+
+  source_tags   = ["web"]
+  source_ranges = ["0.0.0.0/0"]
+}
 
 resource "google_compute_instance" "default" {
   name         = "${local.project_name}-gitlab-instance"
