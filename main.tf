cf08c3221f922b976069590a5e416f6ba132af35
chloe-t <chloe.trouilh@gmail.com>
2023-02-23 08:48:08 +0100

    Remove static ip

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ terraform {
   }
 }
 
-# data "google_client_openid_userinfo" "terraform_service_account" {
-# }
+data "google_client_openid_userinfo" "terraform_service_account" {
+}
 
 resource "tls_private_key" "ephemeral" {
   rsa_bits  = 2048
@@ -32,12 +32,6 @@ locals {
   ssh_user_name                = "chloe_trouilh"
   project_name                 = "i-need-my-belt"
 }
-
-# resource "google_project_iam_member" "project" {
-#   project = local.project_name
-#   role    = "roles/compute.osAdminLogin"
-#   member  = "serviceAccount:${data.google_client_openid_userinfo.terraform_service_account.email}"
-# }
 
 resource "google_compute_resource_policy" "gitlab-instance-scheduler" {
   name        = "gitlab-instance-schedule"
@@ -54,72 +48,13 @@ resource "google_compute_resource_policy" "gitlab-instance-scheduler" {
   }
 }
 
-resource "google_compute_address" "static_ip" {
-  name = "debian-vm"
+resource "google_compute_network" "gitlab-network" {
+  name = "gitlab-network"
 }
 
-output "static_ip" {
-  value = google_compute_address.static_ip.address
-}
-
-resource "google_compute_network" "default" {
-  name = "test-network"
-}
-
-# resource "google_compute_firewall" "ssh" {
-#   name    = "${local.project_name}-firewall-ssh"
-#   network = google_compute_network.default.name
-
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22"]
-#   }
-
-#   target_tags   = ["${local.project_name}-firewall-ssh"]
-#   source_ranges = ["0.0.0.0/0"]
-# }
-
-# resource "google_compute_firewall" "http" {
-#   name    = "${local.project_name}-firewall-http"
-#   network = google_compute_network.default.name
-
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80"]
-#   }
-
-#   target_tags   = ["${local.project_name}-firewall-http"]
-#   source_ranges = ["0.0.0.0/0"]
-# }
-
-# resource "google_compute_firewall" "https" {
-#   name    = "${local.project_name}-firewall-https"
-#   network = google_compute_network.default.name
-
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["443"]
-#   }
-
-#   target_tags   = ["${local.project_name}-firewall-https"]
-#   source_ranges = ["0.0.0.0/0"]
-# }
-
-# resource "google_compute_firewall" "icmp" {
-#   name    = "${local.project_name}-firewall-icmp"
-#   network = google_compute_network.default.name
-
-#   allow {
-#     protocol = "icmp"
-#   }
-
-#   target_tags   = ["${local.project_name}-firewall-icmp"]
-#   source_ranges = ["0.0.0.0/0"]
-# }
-
-resource "google_compute_firewall" "default" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
+resource "google_compute_firewall" "gitlab-network-firewall" {
+  name    = "gitlab-firewall"
+  network = google_compute_network.gitlab-network.name
 
   allow {
     protocol = "icmp"
@@ -155,16 +90,16 @@ resource "google_compute_instance" "default" {
   }
 
   network_interface {
-    network = google_compute_network.default.name
+    network = google_compute_network.gitlab-network.name
     access_config {
-      nat_ip = google_compute_address.static_ip.address
+      # nat_ip = google_compute_address.static_ip.address
     }
   }
 
-  # service_account {
-  #   email  = data.google_client_openid_userinfo.terraform_service_account.email # "github-actions-service-account@i-need-my-belt.iam.gserviceaccount.com"
-  #   scopes = ["cloud-platform"]
-  # }
+  service_account {
+    email  = data.google_client_openid_userinfo.terraform_service_account.email # "github-actions-service-account@i-need-my-belt.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
 
   tags = [
     "${local.project_name}-firewall-ssh",
@@ -186,7 +121,7 @@ resource "google_compute_instance" "default" {
     connection {
       type        = "ssh"
       user        = local.ssh_user_name # gcp user
-      host        = google_compute_address.static_ip.address
+      host        = self.network_interface.0.access_config.0.nat_ip
       timeout     = "500s"
       private_key = local.ssh_private_key
     }
